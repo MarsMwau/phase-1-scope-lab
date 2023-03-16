818c1ebf9498cd3f3a999276da360945ab488386
MarsMwau <marthamumbua16@gmail.com>
2023-03-16 14:54:49 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName () {
+   customerName=customerName.toUpperCase();
+}
+function setBestCustomer() {
+    window.bestCustomer
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return bestCustomer;
+}
+const leastFavoriteCustomer ='Max';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Maxxy'
+    return leastFavoriteCustomer;
+}
